298941d4b79da0e0fd1e083ec02c276ae3c8eb78
Cornellius Ngondo <devngondo@gmail.com>
2015-09-05 14:15:41 +0000

    Conditionals and hello world

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+a, b = 0, 1
+if a < b:
+    print('a({}) is less than b({})'.format(a,b))
+else:
+    print('a({}) is not less than b({})'.format(a,b))
+
+# Condensing the conditionals; conditional expressions
+# Similar to "?" operator in ruby
+print("foo" if a < b else "bar")
